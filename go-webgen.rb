3a3bdb534f68d405a592544b968584d3916afc2c
Hirochi Riveros <hirosmans@gmail.com>
2025-07-07 02:40:07 -0400

    Chore: update new version

--- go-webgen.rb
+++ go-webgen.rb
@@ -1,14 +1,14 @@
 class GoWebgen < Formula
   desc "CLI para generar scaffolds de proyectos web en Go"
   homepage "https://github.com/hiro-riveros/go-webgen"
-  version "1.1.1"
+  version "1.2.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/hiro-riveros/go-webgen/releases/download/1.1.1/go-webgen_Darwin_arm64.tar.gz"
-    sha256 "c19ca9c6c75913d67127eea6464c707607d57ed1c93037c3c1a81eb2e168d679"
+    url "https://github.com/hiro-riveros/go-webgen/releases/download/1.2.0/go-webgen_Darwin_arm64.tar.gz"
+    sha256 "f3623775b203c320208cbaa30b067d293f7f1d186c244ec2e01a364f187b7cff"
   else
-    url "https://github.com/hiro-riveros/go-webgen/releases/download/1.1.1/go-webgen_Darwin_x86_64.tar.gz"
-    sha256 "445806fa2ff934cd4778103b6cf4976c80d7eba128f15729f2f85d870f2998a4"
+    url "https://github.com/hiro-riveros/go-webgen/releases/download/1.2.0/go-webgen_Darwin_x86_64.tar.gz"
+    sha256 "e0570bc159b897288233439ee654ad22394a84af93897b292fe0c7e6bc5f37ce"
   end
 
   def install
